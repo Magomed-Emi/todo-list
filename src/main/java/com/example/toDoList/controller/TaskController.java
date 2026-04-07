f7aaf0a03f48a7ec1fe64632e265679ff9f37c54
Magomed-Emi <Magomed_Emi12@mail.ru>
2026-04-07 14:45:54 +0200

    Finalisation du projet ToDoList

--- src/main/java/com/example/toDoList/controller/TaskController.java
+++ src/main/java/com/example/toDoList/controller/TaskController.java
@@ -1,4 +1,41 @@
 package com.example.toDoList.controller;
 
+import com.example.toDoList.model.Task;
+import com.example.toDoList.service.TaskService;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/tasks")
 public class TaskController {
+
+    private TaskService taskService;
+
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+
+    }
+
+
+    @PostMapping
+    public void ajoutTaches(@RequestParam String titre)
+    {
+        taskService.ajouterTask(titre);
+    }
+    @GetMapping
+    public List<Task> taches()
+    {
+        return taskService.getAllTasks();
+    }
+    @DeleteMapping("/{id}")
+    public void deleteTaches(@PathVariable Long id)
+    {
+        taskService.deleteTask(id);
+    }
+    @PutMapping("/{id}")
+    public void completed(@PathVariable Long id, @RequestParam boolean complete)
+    {
+        taskService.markTaskCompleted(id, complete);
+
+    }
 }
--- src/main/java/com/example/toDoList/service/TaskService.java
+++ src/main/java/com/example/toDoList/service/TaskService.java
@@ -2,54 +2,48 @@ package com.example.toDoList.service;
 
 import com.example.toDoList.model.Task;
 import com.example.toDoList.repository.TaskRepository;
-import java.util.Optional;
-import java.util.List;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class TaskService {
 
     private TaskRepository taskRepository;
 
+    public TaskService(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
 
-    public void ajouterTask(String titre)
-    {
-        if(titre==null)
-        {
+    public void ajouterTask(String titre) {
+        if (titre == null) {
             System.out.println("erreur");
-        }
-        else
-        {
+        } else {
             Task task = new Task(titre);
             taskRepository.save(task);
             System.out.println("Ajouté");
         }
     }
 
-    public List<Task> getAllTasks()
-    {
+    public List<Task> getAllTasks() {
         return taskRepository.findAll();
     }
-    public boolean markTaskCompleted(Long id) {
 
+    public boolean markTaskCompleted(Long id, boolean complete) {
         Optional<Task> tache = taskRepository.findById(id);
 
-        if(tache.isPresent()) {
-
+        if (tache.isPresent()) {
             Task task = tache.get();
-
-            if(task.isCompleted()) {
-                System.out.println("est complété");
-            } else {
-                System.out.println("n'est pas complété");
-            }
-
-            return task.isCompleted();
+            task.setCompleted(complete);
+            taskRepository.save(task);
+            return true;
+        } else {
+            return false;
         }
-
-        return false;
     }
-    public void deleteTask(Long id)
-    {
+
+    public void deleteTask(Long id) {
         taskRepository.deleteById(id);
     }
-
 }
